cd08434cc6465a2ff95a39fb3695397a68bc58c4
Trang NGO <ngotrangdh@gmail.com>
2020-12-08 15:47:37 +0000

    Add Test for StringExtensions

---
+++ tests/expressForm.UnitTest/Shared/Utilities/Extensions/StringExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using expressForm.Shared.Utilities.Extensions;
+
+namespace expressForm.UnitTest.Shared.Utilities.Extensions
+{
+    [TestClass]
+    public class StringExtensionTests
+    {
+        [DataTestMethod]
+        [DataRow("expressForm", "expressForm")]
+        [DataRow(null, "")]
+        public void ToStringOrEmpty(string value, string result)
+        {
+            Assert.AreEqual(result, value.ToStringOrEmpty());
+        }
+        
+    }
+}
