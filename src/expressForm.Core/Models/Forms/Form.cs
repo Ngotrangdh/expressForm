d732b6376b7730beee999fc461e2b48294209c0e
Trang NGO <ngotrangdh@gmail.com>
2020-12-26 12:29:20 +0000

    Add additional property to Form - Guid

--- src/expressForm.Core/Models/Forms/Form.cs
+++ src/expressForm.Core/Models/Forms/Form.cs
@@ -7,6 +7,7 @@ namespace expressForm.Core.Models.Forms
     public class Form
     {
         public int Id { get; set; }
+        public Guid Guid { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Link { get; set; }
--- src/expressForm.Infrastructure/Migrations/20201226122402_Add Guid to Form.Designer.cs
+++ src/expressForm.Infrastructure/Migrations/20201226122402_Add Guid to Form.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using expressForm.Infrastructure;
 
 namespace expressForm.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201226122402_Add Guid to Form")]
+    partial class AddGuidtoForm
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
