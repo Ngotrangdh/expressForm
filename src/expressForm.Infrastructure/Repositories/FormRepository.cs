4e81c2a981515f1bbfbe55f9830eee3adfcc1846
Trang NGO <ngotrangdh@gmail.com>
2020-12-28 21:11:35 +0000

    Add ResponsesController and view

--- src/expressForm.Infrastructure/Repositories/FormRepository.cs
+++ src/expressForm.Infrastructure/Repositories/FormRepository.cs
@@ -28,7 +28,11 @@ namespace expressForm.Infrastructure.Repositories
 
         private async Task<Form> FindAsync(Expression<Func<Form, bool>> predicate)
         {
-            return await _context.Forms.Include(f => f.Questions).SingleOrDefaultAsync(predicate);
+            return await _context.Forms
+                .Include(f => f.Questions)
+                .Include(f => f.Responses)
+                    .ThenInclude(r => r.Answers)
+                .SingleOrDefaultAsync(predicate);
         }
 
         public async Task<IEnumerable<Form>> GetAllAsync()
---
+++ src/expressForm.Web/Controllers/ResponsesController.cs
@@ -0,0 +1,31 @@
+﻿using expressForm.Core.Models.Forms;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace expressForm.Web.Controllers
+{
+    public class ResponsesController : Controller
+    {
+        private readonly IFormRepository _formRepository;
+
+        public ResponsesController(IFormRepository formRepository)
+        {
+            _formRepository = formRepository;
+        }
+
+        public async Task<IActionResult> Index(int? formId)
+        {
+            if (formId == null)
+            {
+                return NotFound();
+            }
+
+            var form = await _formRepository.FindAsync(formId.Value);
+
+            return View(form.Responses);
+        }
+    }
+}
